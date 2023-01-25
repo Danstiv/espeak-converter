981ad92ac3c2b7c379455a5782f7992d3330d509
Danstiv <50794055+Danstiv@users.noreply.github.com>
2023-01-26 02:10:07 +0700

    Use builtin httpx proxy support in downloader

--- downloader.py
+++ downloader.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
-import quart
 import httpx
-from httpx_socks import AsyncProxyTransport
+import quart
+
 from constants import PROXY
+
 app = quart.Quart(__name__)
+
+
 @app.route('/', methods=['POST'])
 async def download():
-	data =await quart.request.json
-	transport = AsyncProxyTransport.from_url(PROXY)
-	client = httpx.AsyncClient(transport=transport, timeout=300)
-	try:
-		response = await client.get(data['url'], headers=data['headers'])
-		await client.aclose()
-	except Exception:
-		response = None
-	if not response:
-		return quart.Response('error', status=500)
-	return quart.Response(response.content, status=response.status_code, headers=dict(response.headers))
+    data =await quart.request.json
+    client = httpx.AsyncClient(proxies=PROXY, timeout=300)
+    try:
+        response = await client.get(data['url'], headers=data['headers'])
+        await client.aclose()
+    except Exception:
+        response = None
+    if not response:
+        return quart.Response('error', status=500)
+    return quart.Response(response.content, status=response.status_code, headers=dict(response.headers))
+
+
 app.run(host='0.0.0.0', port=10011)
