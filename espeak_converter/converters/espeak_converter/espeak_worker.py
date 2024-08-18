f458016b96ded1110f68f5c0891ec1ea210b541f
Danstiv <50794055+Danstiv@users.noreply.github.com>
2024-08-19 03:19:02 +0700

    Decrease gap between chunks

--- espeak_converter/converters/espeak_converter/espeak_worker.py
+++ espeak_converter/converters/espeak_converter/espeak_worker.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import os
 
 from espeak_converter.constants import ESPEAK_DIR, LAME_PATH
 
@@ -31,7 +30,6 @@ class EspeakWorker:
             await self.output_queue.put((id, wav))
 
     async def convert_text(self, text):
-        read_end, write_end = os.pipe()
         espeak = await asyncio.create_subprocess_exec(
             ESPEAK_DIR / "espeak-ng.exe",
             f"--path={ESPEAK_DIR}",
@@ -44,9 +42,10 @@ class EspeakWorker:
             # https://github.com/espeak-ng/espeak-ng/issues/1461#issuecomment-1289315914
             "-s",
             "777",
+            "-z",
             "--stdout",
             stdin=asyncio.subprocess.PIPE,
-            stdout=write_end,
+            stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.DEVNULL,
         )
         lame = await asyncio.create_subprocess_exec(
@@ -55,16 +54,52 @@ class EspeakWorker:
             "96",
             "-",
             "-",
-            stdin=read_end,
+            stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.DEVNULL,
         )
-        espeak_task = asyncio.create_task(espeak.communicate(text.encode()))
-        lame_task = asyncio.create_task(lame.communicate())
-        await espeak_task
-        with open(write_end, "ab") as f:
-            f.flush()
-        stdout, stderr = await lame_task
+
+        async def espeak_write():
+            espeak.stdin.write(text.encode())
+            await espeak.stdin.drain()
+            espeak.stdin.close()
+            await espeak.stdin.wait_closed()
+
+        espeak_write_task = asyncio.create_task(espeak_write())
+
+        async def lame_read():
+            return await lame.stdout.read()
+
+        lame_read_task = asyncio.create_task(lame_read())
+        previous_wav_chunk = b""
+        while espeak.returncode is None:
+            wav_chunk = await espeak.stdout.read(65536)
+            if not wav_chunk:
+                continue
+            if previous_wav_chunk:
+                lame.stdin.write(previous_wav_chunk)
+                await lame.stdin.drain()
+            else:
+                # Start of stream.
+                header = wav_chunk[:44]
+                stripped_wav_data = self.strip_zeros_from_pcm_chunk(
+                    wav_chunk[44:], left=True
+                )
+                wav_chunk = header + stripped_wav_data
+            previous_wav_chunk = wav_chunk
+        previous_wav_chunk += await espeak.stdout.read()
+        if previous_wav_chunk:
+            previous_wav_chunk = self.strip_zeros_from_pcm_chunk(
+                previous_wav_chunk, right=True
+            )
+            lame.stdin.write(previous_wav_chunk)
+            await lame.stdin.drain()
+        lame.stdin.close()
+        await lame.stdin.wait_closed()
+        await espeak_write_task
+        await espeak.wait()
+        await lame.wait()
+        stdout = await lame_read_task
         if espeak.returncode != 0 or lame.returncode != 0:
             logger.error("Chunk convertion failed!")
             return self.generate_placeholder()
@@ -72,3 +107,18 @@ class EspeakWorker:
 
     def generate_placeholder(self):
         return b"\x00" * 40000
+
+    @staticmethod
+    def strip_zeros_from_pcm_chunk(chunk, *, left=False, right=False):
+        stripped_chunk = chunk
+        if left:
+            stripped_chunk = chunk.lstrip(b"\x00")
+            delta = len(chunk) - len(stripped_chunk)
+            if delta % 2:
+                stripped_chunk = b"\x00" + stripped_chunk
+        if right:
+            stripped_chunk = chunk.rstrip(b"\x00")
+            delta = len(chunk) - len(stripped_chunk)
+            if delta % 2:
+                stripped_chunk = stripped_chunk + b"\x00"
+        return stripped_chunk
