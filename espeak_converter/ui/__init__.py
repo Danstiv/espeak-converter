b135c08d68d70385a949a7137fc6abc4fd4ee89a
Danstiv <50794055+Danstiv@users.noreply.github.com>
2024-08-21 01:39:05 +0700

    Revert "Fix max jobs requirements in ui"
    This reverts commit 6416f5679b924dcdb230938407f55349093f445c.

--- espeak_converter/ui/__init__.py
+++ espeak_converter/ui/__init__.py
@@ -86,6 +86,7 @@ class UI:
         prompt = (
             "Максимальное число потоков.\n"
             f"Минимум 2, максимально рекомендовано {total_cpu}.\n"
+            "Число потоков должно быть чётным.\n"
             "Введите число потоков: "
         )
         while True:
@@ -95,6 +96,9 @@ class UI:
                 continue
             if answer < 2:
                 print("2 - минимальное значение")
+                continue
+            if answer % 2:
+                print("Число потоков должно быть чётным.")
                 continue
             break
         if answer > total_cpu:
